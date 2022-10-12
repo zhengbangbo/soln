0cfdca34759b78231ec23b12026b23aad3eabd7f
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-10-12 13:29:00 +0800

    soln(vuejs): dom portal

---
+++ vuejs-challenges/03-easy-dom-portal/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+const msg = 'Hello World'
+</script>
+
+<template>
+  <!-- Renders it to a child element of the `body` -->
+  <Teleport to="body">
+    <span>{{ msg }}</span>
+  </Teleport>
+</template>
