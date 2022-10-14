5149584b9cebf9f80ffbbdf6ebaa36013ce88de5
Bang-Bo Zheng <zhengbangbo@hotmail.com>
2022-10-14 13:10:12 +0800

    soln(vuejs): v-model .capitalize

---
+++ vuejs-challenges/08-easy-capitalize/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+import MyComponent from './MyComponent.vue'
+const value = ref('')
+</script>
+
+<template>
+  <MyComponent v-model.capitalize="value" />
+</template>
---
+++ vuejs-challenges/08-easy-capitalize/MyComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+const props = defineProps({
+  modelValue: String,
+  modelModifiers: { default: () => ({}) },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function emitValue(e) {
+  let value = e.target.value
+  if (props.modelModifiers.capitalize)
+    value = value.charAt(0).toUpperCase() + value.slice(1)
+
+  emit('update:modelValue', value)
+}
+</script>
+
+<template>
+  <input type="text" :value="modelValue" @input="emitValue">
+</template>
